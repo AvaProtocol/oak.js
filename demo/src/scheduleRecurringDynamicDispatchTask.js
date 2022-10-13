4ae6656e876cd1541df194459582463e928fa7e2
imstar15 <imstar15@qq.com>
2022-10-14 00:43:23 +0800

    Add demo

---
+++ demo/src/scheduleRecurringDynamicDispatchTask.js
@@ -0,0 +1,60 @@
+require('@oak-foundation/api-augment');
+const { rpc, types } = require('@oak-foundation/types');
+const { ApiPromise, WsProvider, Keyring } = require('@polkadot/api');
+const { waitReady } = require('@polkadot/wasm-crypto');
+const _ = require('lodash');
+
+const SS58_PREFIX = 51;
+const TRANSFER_AMOUNT = 1000000000;
+const RECEIVER_ADDRESS = '66fhJwYLiK87UDXYDQP9TfYdHpeEyMvQ3MK8Z6GgWAdyyCL3';
+
+const getHourlyRecurringTimestamps = (startTimestamp, numberRecurring) => {
+  const secondsInHour = 60 * 60 * 1000;
+  const firstEventTimestamp = startTimestamp - (startTimestamp % secondsInHour) + secondsInHour;
+  return _.times(numberRecurring, (index) => {
+      return firstEventTimestamp + index * secondsInHour;
+  });
+}
+
+const getKeyringPair = async () => {
+  await waitReady();
+  if (_.isEmpty(process.env.SENDER_MNEMONIC)) {
+    throw new Error('The SENDER_MNEMONIC environment variable is not set.')
+  }
+  // Generate sender keyring pair from mnemonic
+  const keyring = new Keyring({ type: 'sr25519', ss58Format: SS58_PREFIX });
+  const keyringPair = keyring.addFromMnemonic(process.env.SENDER_MNEMONIC);
+  return keyringPair;
+}
+
+const sendExtrinsic = (extrinsic, keyringPair) => {
+  return new Promise(async (resolve) => {
+    const unsub = await extrinsic.signAndSend(keyringPair, (result)=> {
+      const { status } = result;
+      console.log('status.type: ', status.type);
+      if (status.isFinalized) {
+        unsub();
+        resolve();
+      }
+    });
+  });
+}
+
+async function main() {
+  const providerUrl = process.env.PROVIDER_URL;
+  
+  const provider = new WsProvider(providerUrl);
+  const api = await ApiPromise.create({ provider, types, rpc });
+  const keyringPair = await getKeyringPair();
+
+	// Create dynamic dispatch task and send
+	const providedId = `demo-${new Date().getTime()}-${_.random(0, Number.MAX_SAFE_INTEGER, false)}`;
+  const executionTimes = _.map(getHourlyRecurringTimestamps(new Date().valueOf(), 1), (time) => time / 1000);
+  const schedule = { recurring: { nextExecutionTime: executionTimes[0], frequency: 3600 }};
+  const call = api.tx.balances.transfer(RECEIVER_ADDRESS, TRANSFER_AMOUNT);
+
+  const extrinsic = api.tx.automationTime.scheduleDynamicDispatchTask(providedId, schedule, call);
+  await sendExtrinsic(extrinsic, keyringPair);
+}
+
+main().catch(console.error).finally(() => process.exit()); 

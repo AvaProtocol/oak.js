b7d011bead899a123b3853b935a004b519d7727c
Vinh <vinh@oak.tech>
2023-08-04 19:32:15 -0700

    remove more generate task id definiton

--- packages/types/src/xcmpHandler.ts
+++ packages/types/src/xcmpHandler.ts
@@ -20,11 +20,6 @@ export default {
     XcmpHandlerApi: [
       {
         methods: {
-          generate_task_id: {
-            description: 'Find xcmp account id',
-            params: [{ name: 'accountId', type: 'AccountId32' }],
-            type: 'AccountId32'
-          },
           get_time_automation_fees: {
             description: 'Determine fees for a scheduled xcmp task',
             params: [{ name: 'encodedXt', type: 'Bytes' }],

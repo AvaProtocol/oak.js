7400ef8e3c7cedaca71896eea9038d3252279bfe
chrisli30 <chrisli30@users.noreply.github.com>
2023-08-22 22:29:51 -0700

    Added script to test workflow

---
+++ .github/scripts/update-pr-body.js
@@ -0,0 +1,25 @@
+async function updatePullRequest() {
+    const githubToken = process.env.GITHUB_TOKEN;
+    const octokit = new GitHub(githubToken);
+  
+    const prNumber = context.payload.pull_request.number;
+    const prOwner = context.payload.repository.owner.login;
+    const prRepo = context.payload.repository.name;
+    const prBody = context.payload.pull_request.body;
+  
+    const changesetUpdates = `${prBody}\nUpdate changeset information here`;
+  
+    await octokit.pulls.update({
+      owner: prOwner,
+      repo: prRepo,
+      pull_number: prNumber,
+      body: changesetUpdates,
+    });
+  
+    console.log('Pull request updated with changeset information.');
+  }
+  
+  updatePullRequest().catch(error => {
+    console.error('Error updating pull request:', error);
+    process.exit(1);
+  });

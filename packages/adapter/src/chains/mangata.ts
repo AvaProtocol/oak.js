283a0b96573326f99bbbffb37053c273e93066fa
imstar15 <imstar15@qq.com>
2023-09-30 02:06:19 +0800

    Custom SDK options (#82)
    * Custom SDK options
    
    * Remove options
    
    * Fix test

--- packages/adapter/src/chains/mangata.ts
+++ packages/adapter/src/chains/mangata.ts
@@ -1,19 +1,15 @@
-import _ from "lodash";
-import BN from "bn.js";
-import type { SubmittableExtrinsic, AddressOrPair } from "@polkadot/api/types";
-import type { u32, u64, u128, Option } from "@polkadot/types";
-import type { WeightV2 } from "@polkadot/types/interfaces";
-import type { HexString } from "@polkadot/util/types";
-import type { KeyringPair } from "@polkadot/keyring/types";
-import { Weight } from "@oak-network/sdk-types";
-import { ChainAdapter } from "./chainAdapter";
-import {
-  convertAbsoluteLocationToRelative,
-  getDerivativeAccountV2,
-  sendExtrinsic,
-} from "../util";
-import { WEIGHT_REF_TIME_PER_SECOND } from "../constants";
-import { SendExtrinsicResult } from "../types";
+import _ from 'lodash';
+import BN from 'bn.js';
+import type { SubmittableExtrinsic, AddressOrPair } from '@polkadot/api/types';
+import type { u32, u64, u128, Option } from '@polkadot/types';
+import type { WeightV2 } from '@polkadot/types/interfaces';
+import type { HexString } from '@polkadot/util/types';
+import type { KeyringPair } from '@polkadot/keyring/types';
+import { Weight } from '@oak-network/sdk-types';
+import { ChainAdapter } from './chainAdapter';
+import { convertAbsoluteLocationToRelative, getDerivativeAccountV2, sendExtrinsic } from '../util';
+import { WEIGHT_REF_TIME_PER_SECOND } from '../constants';
+import { SendExtrinsicResult } from '../types';
 
 // MangataAdapter implements ChainAdapter
 export class MangataAdapter extends ChainAdapter {
@@ -25,82 +21,64 @@ export class MangataAdapter extends ChainAdapter {
   }
 
   /**
-   * Get extrinsic weight
+   * Get extrinsic weight for transact an extrinsic call through XCM message
    * @param extrinsic
    * @param account
    * @returns Extrinsic weight
    */
-  async getExtrinsicWeight(
-    extrinsic: SubmittableExtrinsic<"promise">,
-    account: AddressOrPair,
-  ): Promise<Weight> {
-    const { refTime, proofSize } = (await extrinsic.paymentInfo(account))
-      .weight as unknown as WeightV2;
+  async getExtrinsicWeight(extrinsic: SubmittableExtrinsic<'promise'>, account: AddressOrPair): Promise<Weight> {
+    const { refTime, proofSize } = (await extrinsic.paymentInfo(account)).weight as unknown as WeightV2;
     return new Weight(new BN(refTime.unwrap()), new BN(proofSize.unwrap()));
   }
 
   /**
-   * Calculate encoded call weight and overall weight for transact an extrinsic call through XCM message
-   * @param extrinsic The extrinsic that needs to be transacted
-   * @param account
+   * Calculate XCM overall weight for transact an extrinsic call through XCM message
+   * @param transactCallWeight
    * @param instructionCount The number of XCM instructions
-   * returns { encodedCallWeight, overallWeight }
+   * @returns XCM overall weight
    */
-  async getXcmWeight(
-    extrinsic: SubmittableExtrinsic<"promise">,
-    account: AddressOrPair,
-    instructionCount: number,
-  ): Promise<{ encodedCallWeight: Weight; overallWeight: Weight }> {
+  async calculateXcmOverallWeight(transactCallWeight: Weight, instructionCount: number): Promise<Weight> {
     const { xcm } = this.chainData;
     if (_.isUndefined(xcm)) throw new Error("chainData.xcm not set");
-    const encodedCallWeight = await this.getExtrinsicWeight(extrinsic, account);
-    const overallWeight = encodedCallWeight.add(
-      xcm.instructionWeight?.muln(instructionCount),
-    );
-    return { encodedCallWeight, overallWeight };
+    const overallWeight = transactCallWeight.add(xcm.instructionWeight.muln(instructionCount));
+    return overallWeight;
   }
 
   /**
    * Calculate XCM execution fee based on weight
-   * @param weight
-   * @param assetLocation
+   * @param weight 
+   * @param assetLocation 
    * @returns XCM execution fee
    */
   async weightToFee(weight: Weight, assetLocation: any): Promise<BN> {
     const { defaultAsset } = this.chainData;
-    if (_.isUndefined(defaultAsset))
-      throw new Error("chainData.defaultAsset not set");
+    if (_.isUndefined(defaultAsset)) throw new Error("chainData.defaultAsset not set");
 
     const api = this.getApi();
     if (_.isEqual(defaultAsset.location, assetLocation)) {
-      const fee = (await api.call.transactionPaymentApi.queryWeightToFee(
-        weight,
-      )) as u64;
+      const fee = await api.call.transactionPaymentApi.queryWeightToFee(weight) as u64;
       return fee;
+    } else {
+      const storageValue = await api.query.assetRegistry.locationToAssetId(assetLocation);
+      const item = storageValue as unknown as Option<u32>;
+      if (item.isNone) throw new Error("AssetTypeUnitsPerSecond is null");
+
+      const assetId = item.unwrap();
+      const metadataStorageValue = await api.query.assetRegistry.metadata(assetId);
+      const metadataItem = metadataStorageValue as unknown as Option<any>;
+      if (metadataItem.isNone) throw new Error("Metadata is null");
+
+      const { additional: { xcm } } = metadataItem.unwrap() as { additional: { xcm: Option<any> } };
+      if (_.isUndefined(xcm)) throw new Error("Metadata additional.xcm is null");
+      const feePerSecond = xcm.unwrap().feePerSecond as u128;
+      
+      return weight.refTime.mul(feePerSecond).div(WEIGHT_REF_TIME_PER_SECOND);
     }
-    const storageValue =
-      await api.query.assetRegistry.locationToAssetId(assetLocation);
-    const item = storageValue as unknown as Option<u32>;
-    if (item.isNone) throw new Error("AssetTypeUnitsPerSecond is null");
-
-    const assetId = item.unwrap();
-    const metadataStorageValue =
-      await api.query.assetRegistry.metadata(assetId);
-    const metadataItem = metadataStorageValue as unknown as Option<any>;
-    if (metadataItem.isNone) throw new Error("Metadata is null");
-
-    const {
-      additional: { xcm },
-    } = metadataItem.unwrap() as { additional: { xcm: Option<any> } };
-    if (_.isUndefined(xcm)) throw new Error("Metadata additional.xcm is null");
-    const feePerSecond = xcm.unwrap().feePerSecond as u128;
-
-    return weight.refTime.mul(feePerSecond).div(WEIGHT_REF_TIME_PER_SECOND);
   }
 
   /**
    * Calculate the derivative account ID of a certain account ID
-   * @param accountId
+   * @param accountId 
    * @param paraId The paraId of the XCM message sender
    * @returns Derivative account
    */
@@ -111,14 +89,13 @@ export class MangataAdapter extends ChainAdapter {
 
   /**
    * Check if it is a native asset
-   * @param assetLocation
+   * @param assetLocation 
    * @returns A bool value indicating whether it is a native asset
    */
   isNativeAsset(assetLocation: any): boolean {
     const { defaultAsset, assets } = this.chainData;
-    if (_.isUndefined(defaultAsset))
-      throw new Error("chainData.defaultAsset not set");
-    const foundAsset = _.find(assets, { location: assetLocation });
+    if (_.isUndefined(defaultAsset)) throw new Error('chainData.defaultAsset not set');
+    const foundAsset = _.find(assets, ({ location: assetLocation }));
     return !_.isUndefined(foundAsset) && foundAsset.isNative;
   }
 
@@ -131,16 +108,10 @@ export class MangataAdapter extends ChainAdapter {
    * @param keyringPair Operator's keyring pair
    * @returns SendExtrinsicResult
    */
-  async crossChainTransfer(
-    destination: any,
-    recipient: HexString,
-    assetLocation: any,
-    assetAmount: BN,
-    keyringPair: KeyringPair,
-  ): Promise<SendExtrinsicResult> {
+  async crossChainTransfer(destination: any, recipient: HexString, assetLocation: any, assetAmount: BN, keyringPair: KeyringPair): Promise<SendExtrinsicResult> {
     const { key } = this.chainData;
-    if (_.isUndefined(key)) throw new Error("chainData.key not set");
-
+    if (_.isUndefined(key)) throw new Error('chainData.key not set');
+    
     const transferAssetLocation = this.isNativeAsset(assetLocation)
       ? convertAbsoluteLocationToRelative(assetLocation)
       : assetLocation;
@@ -162,9 +133,9 @@ export class MangataAdapter extends ChainAdapter {
               { AccountId32: { network: null, id: recipient } },
             ],
           },
-        },
+        }
       },
-      "Unlimited",
+      'Unlimited',
     );
 
     console.log(`Transfer from ${key}, extrinsic:`, extrinsic.method.toHex());

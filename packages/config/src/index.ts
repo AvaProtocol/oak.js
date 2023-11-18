a69fa8ca09edc94a6235187b4f77d0bafc77a96f
chrisli30 <chrisli30@users.noreply.github.com>
2023-11-17 23:08:26 -0800

    Changed the format of config export

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,2 +1,7 @@
-export * from "./tokens";
-export * from "./chains";
+// Importing the variables from the respective files
+import { DevTokens, MoonbaseTokens, RococoTokens, KusamaTokens, PolkadotTokens } from "./tokens";
+import { devChains, moonbaseChains, rococoChains, kusamaChains, polkadotChains } from "./chains";
+
+// Exporting them as objects within an object
+export const tokens = { DevTokens, MoonbaseTokens, RococoTokens, KusamaTokens, PolkadotTokens };
+export const chains = { devChains, moonbaseChains, rococoChains, kusamaChains, polkadotChains };

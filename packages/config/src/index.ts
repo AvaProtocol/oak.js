645147e80aadce9bcc412665acfff3b2af08922e
chrisli30 <chrisli30@users.noreply.github.com>
2023-11-19 15:38:42 -0800

    Updated config’s index.ts to export types

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,7 +1,15 @@
-// Importing the variables from the respective files
+// Importing variables
+import { DevChains, MoonbaseChains, RococoChains, KusamaChains, PolkadotChains } from "./chains";
 import { DevTokens, MoonbaseTokens, RococoTokens, KusamaTokens, PolkadotTokens } from "./tokens";
-import { devChains, moonbaseChains, rococoChains, kusamaChains, polkadotChains } from "./chains";
 
-// Exporting them as objects within an object
+// Importing types
+import type { Chain, XcmConfig, Weight } from "./chains";
+import type { XToken } from "./tokens";;
+
+// Exporting variables as objects within tokens and chains
+export const chains = { DevChains, MoonbaseChains, RococoChains, KusamaChains, PolkadotChains };
 export const tokens = { DevTokens, MoonbaseTokens, RococoTokens, KusamaTokens, PolkadotTokens };
-export const chains = { devChains, moonbaseChains, rococoChains, kusamaChains, polkadotChains };
+
+// Exporting types
+export type { Chain, XcmConfig, Weight };
+export type { XToken };

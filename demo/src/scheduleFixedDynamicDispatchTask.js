236bec687da12206cbf7694bde97c93c4de50e2d
imstar15 <imstar15@qq.com>
2023-11-21 22:26:36 +0800

    Fix dynamic task demo

--- demo/src/scheduleFixedDynamicDispatchTask.js
+++ demo/src/scheduleFixedDynamicDispatchTask.js
@@ -2,17 +2,23 @@ require("@oak-network/api-augment");
 const { rpc, types } = require("@oak-network/types");
 const { ApiPromise, WsProvider, Keyring } = require("@polkadot/api");
 const { waitReady } = require("@polkadot/wasm-crypto");
-const _ = require("lodash");
+const lodash = require("lodash");
 
 const SS58_PREFIX = 51;
 const TRANSFER_AMOUNT = 1000000000;
 const RECEIVER_ADDRESS = "66fhJwYLiK87UDXYDQP9TfYdHpeEyMvQ3MK8Z6GgWAdyyCL3";
 
+const findEvent = (events, section, method) =>
+  events.find((e) => e.event.section === section && e.event.method === method);
+
+const getTaskIdInTaskScheduledEvent = (event) =>
+  Buffer.from(event.event.data.taskId).toString();
+
 const getHourlyRecurringTimestamps = (startTimestamp, numberRecurring) => {
   const secondsInHour = 60 * 60 * 1000;
   const firstEventTimestamp =
     startTimestamp - (startTimestamp % secondsInHour) + secondsInHour;
-  return _.times(
+  return lodash.times(
     numberRecurring,
     (index) => firstEventTimestamp + index * secondsInHour,
   );
@@ -20,40 +26,44 @@ const getHourlyRecurringTimestamps = (startTimestamp, numberRecurring) => {
 
 const getKeyringPair = async () => {
   await waitReady();
-  if (_.isEmpty(process.env.SENDER_MNEMONIC)) {
+  if (lodash.isEmpty(process.env.SENDER_MNEMONIC)) {
     throw new Error("The SENDER_MNEMONIC environment variable is not set.");
   }
   // Generate sender keyring pair from mnemonic
-  const keyring = new Keyring({ type: "sr25519", ss58Format: SS58_PREFIX });
+  const keyring = new Keyring({ ss58Format: SS58_PREFIX, type: "sr25519" });
   const keyringPair = keyring.addFromMnemonic(process.env.SENDER_MNEMONIC);
   return keyringPair;
 };
 
 const sendExtrinsic = (extrinsic, api, keyringPair) =>
-  new Promise(async (resolve) => {
-    const unsub = await extrinsic.signAndSend(keyringPair, (result) => {
-      const { status, events, dispatchError } = result;
-      console.log("status.type: ", status.type);
+  new Promise((resolve, reject) => {
+    const signAndSend = async () => {
+      const unsub = await extrinsic.signAndSend(keyringPair, (result) => {
+        const { status, events, dispatchError } = result;
+        console.log("status.type: ", status.type);
 
-      if (status?.isFinalized) {
-        unsub();
-        if (!_.isNil(dispatchError)) {
-          reject(dispatchError);
-        }
+        if (status?.isFinalized) {
+          unsub();
+          if (!lodash.isNil(dispatchError)) {
+            reject(dispatchError);
+          }
 
-        const event = _.find(events, ({ event }) =>
-          api.events.system.ExtrinsicSuccess.is(event),
-        );
-        if (event) {
-          resolve({
-            extrinsicHash: extrinsic.hash,
-            blockHash: status?.asFinalized?.toString(),
-          });
-        } else {
-          reject(new Error("The event.ExtrinsicSuccess is not found"));
+          const event = lodash.find(events, ({ event: eventItem }) =>
+            api.events.system.ExtrinsicSuccess.is(eventItem),
+          );
+          if (event) {
+            resolve({
+              blockHash: status?.asFinalized?.toString(),
+              events,
+              extrinsicHash: extrinsic.hash,
+            });
+          } else {
+            reject(new Error("The event.ExtrinsicSuccess is not found"));
+          }
         }
-      }
-    });
+      });
+    };
+    signAndSend();
   });
 
 async function main() {
@@ -61,16 +71,10 @@ async function main() {
     process.env.PROVIDER_URL || "wss://rpc.turing-staging.oak.tech";
 
   const provider = new WsProvider(providerUrl);
-  const api = await ApiPromise.create({ provider, types, rpc });
+  const api = await ApiPromise.create({ provider, rpc, types });
   const keyringPair = await getKeyringPair();
 
-  // Prepare extrinsic parameters
-  const providedId = `demo-${new Date().getTime()}-${_.random(
-    0,
-    Number.MAX_SAFE_INTEGER,
-    false,
-  )}`;
-  const executionTimes = _.map(
+  const executionTimes = lodash.map(
     getHourlyRecurringTimestamps(new Date().valueOf(), 5),
     (time) => time / 1000,
   );
@@ -79,11 +83,10 @@ async function main() {
 
   // Create dynamic dispatch task and send
   const extrinsic = api.tx.automationTime.scheduleDynamicDispatchTask(
-    providedId,
     schedule,
     call,
   );
-  const { extrinsicHash, blockHash } = await sendExtrinsic(
+  const { extrinsicHash, events, blockHash } = await sendExtrinsic(
     extrinsic,
     api,
     keyringPair,
@@ -93,11 +96,9 @@ async function main() {
   );
 
   // Get task ID
-  const taskIdCodec = await api.rpc.automationTime.generateTaskId(
-    keyringPair.address,
-    providedId,
-  );
-  console.log("taskId: ", taskIdCodec.toString());
+  const event = findEvent(events, "automationTime", "TaskScheduled");
+  const taskId = getTaskIdInTaskScheduledEvent(event);
+  console.log("taskId: ", taskId);
 }
 
 main()

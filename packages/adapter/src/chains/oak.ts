af754b2eb58ff86fb7a15670f40d540ec4cb9fd5
imstar15 <imstar15@qq.com>
2023-11-23 21:42:45 +0800

    Auto compound interfaces

--- packages/adapter/src/chains/oak.ts
+++ packages/adapter/src/chains/oak.ts
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import BN from "bn.js";
+import { u8aToHex } from "@polkadot/util";
 import type { HexString } from "@polkadot/util/types";
 import type { SubmittableExtrinsic, AddressOrPair } from "@polkadot/api/types";
 import type { u32, u128, Option } from "@polkadot/types";
@@ -203,5 +204,200 @@ export class OakAdapter extends ChainAdapter {
   ): HexString {
     const api = this.getApi();
     return getDerivativeAccountV2(api, accountId, paraId, options);
+  }
+
+  // Get delegator state
+  async getDelegatorState(delegator: HexString): Promise<any> {
+    const api = this.getApi();
+    const delegatorStateCodec =
+      await api.query.parachainStaking.delegatorState(delegator);
+    const delegatorState = delegatorStateCodec as unknown as Option<any>;
+    return delegatorState.isSome ? delegatorState.unwrap() : undefined;
+  }
+
+  /**
+   * Get delegation
+   * @param delegator
+   * @param collator
+   * @returns
+   */
+  async getDelegation(delegator: HexString, collator: HexString): Promise<any> {
+    const delegatorState = await this.getDelegatorState(delegator);
+    if (_.isUndefined(delegatorState)) {
+      return undefined;
+    }
+    const { delegations } = delegatorState;
+    const foundDelegation = _.find(
+      delegations,
+      ({ owner }) => owner.toHex() === collator,
+    );
+    return foundDelegation;
+  }
+
+  /**
+   * Get auto-compounding delegations length
+   * @param collator
+   * @returns
+   */
+  async getAutoCompoundingDelegationsLength(
+    collator: HexString,
+  ): Promise<number> {
+    const api = this.getApi();
+    const autoCompoundingDelegationsCodec =
+      await api.query.parachainStaking.autoCompoundingDelegations(collator);
+    const autoCompoundingDelegations =
+      autoCompoundingDelegationsCodec as unknown as Option<any>;
+    let autoCompoundingDelegationsLength = 0;
+    if (autoCompoundingDelegations.isSome) {
+      const { delegations } = autoCompoundingDelegations.unwrap();
+      autoCompoundingDelegationsLength = delegations.length;
+    }
+    return autoCompoundingDelegationsLength;
+  }
+
+  /**
+   * Delegate to collator with auto-compounding
+   * @param collator
+   * @param amount
+   * @param percentage
+   * @param keyringPair
+   * @returns
+   */
+  async delegateWithAutoCompound(
+    collator: HexString,
+    amount: BN,
+    percentage: number,
+    keyringPair: KeyringPair,
+  ): Promise<SendExtrinsicResult> {
+    const api = this.getApi();
+    // Check if delegation exists
+    const delegatorWalletAddress = u8aToHex(keyringPair.addressRaw);
+    const delegatorState = await this.getDelegatorState(delegatorWalletAddress);
+    if (_.isUndefined(delegatorState)) {
+      throw new Error("Delegator state not found");
+    }
+    const { delegations } = delegatorState;
+    const foundDelegation = _.find(
+      delegations,
+      ({ owner }) => owner.toHex() === collator,
+    );
+    const { length: delegationsLength } = delegations;
+    if (!_.isUndefined(foundDelegation)) {
+      throw new Error("Delegation already exists");
+    }
+
+    const { minDelegationCodec } = api.consts.parachainStaking;
+    const minDelegation = minDelegationCodec as u128;
+    if (amount.lt(minDelegation)) {
+      throw new Error(
+        `Amount must be greater than or equal to ${minDelegation}`,
+      );
+    }
+
+    const candidateInfoCodec =
+      await api.query.parachainStaking.candidateInfo(collator);
+    const candidateInfo = candidateInfoCodec as unknown as Option<any>;
+    if (candidateInfo.isSome) {
+      throw new Error("Candidate info not found");
+    }
+    const { delegationCount: candidateDelegationCount } =
+      candidateInfo.unwrap();
+
+    const autoCompoundingDelegationsLength =
+      this.getAutoCompoundingDelegationsLength(collator);
+
+    // Delegate to collator
+    const delegateExtrinsic = api.tx.parachainStaking.delegateWithAutoCompound(
+      collator,
+      amount,
+      percentage,
+      candidateDelegationCount,
+      autoCompoundingDelegationsLength,
+      delegationsLength,
+    );
+
+    const result = await sendExtrinsic(api, delegateExtrinsic, keyringPair);
+    return result;
+  }
+
+  /**
+   * Get auto-compounding delegation percentage
+   * @param delegator
+   * @param collator
+   * @returns
+   */
+  async getAutoCompoundingDelegationPercentage(
+    delegator: HexString,
+    collator: HexString,
+  ): Promise<number> {
+    const api = this.getApi();
+    const autoCompoundingDelegationsCodec =
+      await api.query.parachainStaking.autoCompoundingDelegations(collator);
+    const autoCompoundingDelegations =
+      autoCompoundingDelegationsCodec as unknown as Option<any>;
+    const delegation = _.find(autoCompoundingDelegations, { delegator });
+    return _.isUndefined(delegation) ? 0 : delegation.percentage.toNumber();
+  }
+
+  /**
+   * Set auto-compounding delegation percentage
+   * @param collator
+   * @param percentage
+   * @param keyringPair
+   * @returns
+   */
+  async setAutoCompound(
+    collator: HexString,
+    percentage: number,
+    keyringPair: KeyringPair,
+  ): Promise<SendExtrinsicResult> {
+    const api = this.getApi();
+    // const delegatorWalletAddress = u8aToHex(keyringPair.addressRaw);
+    const setAutoCompoundExtrinsic = api.tx.parachainStaking.setAutoCompound(
+      collator,
+      percentage,
+    );
+    const result = await sendExtrinsic(
+      api,
+      setAutoCompoundExtrinsic,
+      keyringPair,
+    );
+    return result;
+  }
+
+  /**
+   * Bond more
+   * @param collator
+   * @param amount
+   * @param keyringPair
+   * @returns
+   */
+  async bondMore(
+    collator: HexString,
+    amount: BN,
+    keyringPair: KeyringPair,
+  ): Promise<SendExtrinsicResult> {
+    const api = this.getApi();
+
+    const autoCompoundingDelegationsLength =
+      await this.getAutoCompoundingDelegationsLength(collator);
+
+    const delegatorState = await this.getDelegatorState(collator);
+    if (_.isUndefined(delegatorState)) {
+      throw new Error("Delegator state not found");
+    }
+    const { delegations } = delegatorState;
+    const { length: delegationsLength } = delegations;
+
+    // Create bondMoreExtrinsic
+    const bondMoreExtrinsic = api.tx.parachainStaking.delegatorBondMore(
+      collator,
+      amount,
+      autoCompoundingDelegationsLength + 1,
+      delegationsLength + 1,
+    );
+
+    const result = await sendExtrinsic(api, bondMoreExtrinsic, keyringPair);
+    return result;
   }
 }
